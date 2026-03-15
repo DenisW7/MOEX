0fddf25da8d9e5607d3407986182190b5bbce7fc
DenisW7 <denis2017popow@mail.ru>
2026-03-15 13:04:39 +0300

    установил библиотеку lxml

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import xml
 
 url = requests.get('https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities.xml?iss.dp=comma&iss.meta=off&iss.only=marketdata&marketdata.columns=SECID,LAST')
-
-print(url.status_code)
-
-obj_list = ['Hello', 'Joy', 'Denis']
-
-print('{0} {2}. I\'m {1}'.format(*obj_list))
