629aa925190e60d4be0e1c75f6b6c2aaa9e3d8c1
DenisW7 <denis2017popow@mail.ru>
2026-03-15 12:53:04 +0300

    Добавил список и его вывод

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ import time
 url = requests.get('https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities.xml?iss.dp=comma&iss.meta=off&iss.only=marketdata&marketdata.columns=SECID,LAST')
 
 print(url.status_code)
+
+obj_list = ['Hello', 'Joy', 'Denis']
+
+print('{0} {2}. I\'m {1}'.format(*obj_list))
